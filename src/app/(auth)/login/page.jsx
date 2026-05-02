1d4f4658276625458a012b842038bf2ed79ecd8d
LingkonDash <cocrupon@gmail.com>
2026-05-02 19:31:29 +0600

    added login page

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,10 +1,13 @@
 import bgImage2 from '@/assets/hero-bg/newBG4.png'
+import LoginForm from '@/components/form/LoginForm'
 
 export default function LoginPage() {
     return (
         <div className="text-center relative min-h-screen flex justify-center items-center bg-center bg-cover w-full py-20" style={{ backgroundImage: `url(${bgImage2.src})` }}>
-            <div className="absolute inset-0 bg-black/30 backdrop-blur-sm" />
-            make here a glass style login form
+            <div className="absolute inset-0 bg-black/40 backdrop-blur-md" />
+            <div className='z-10 block'>
+                <LoginForm />
+            </div>
         </div>
     )
 }
